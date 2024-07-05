a7ca6a91806fb86238ab6d5c1743598b881da54e
Liza Kobtseva <elizavetakobtsevaa@yandex.ru>
2024-07-05 12:24:58 +0300

    button

--- aeron/src/App.js
+++ aeron/src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sendMessage = () => {
+    fetch('http://localhost:5000/download', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message_id: 1,
+        message: 'Hello, world!',
+        result: 'Success',
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
